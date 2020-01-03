d3e0a4a9780925cde62ba1b6e8263e02d747e6d8
AllenSun7 <sunlun7@yahoo.com>
2020-01-02 16:50:39 -0800

    multiple and normal

--- ex1/test.py
+++ ex1/test.py
@@ -5,9 +5,9 @@ def test():
     #l = range(20)
     l = [[1, 3], [4, 5], [5, 7]]
     lis =  np.asarray([(0, 1, 2), (3, 4, 5), (6, 7, 8)])
-    x = lis[1, :]
+    x = lis[1,]
     y = lis[:2]
-    z = lis[:1,2]
+    z = lis[:, 1]
     print(lis)
     print(x)
     print(y)
