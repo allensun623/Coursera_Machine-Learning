4847e2dc600f7ae6934973988904663f922d1f0c
AllenSun7 <sunlun7@yahoo.com>
2020-01-04 23:00:08 -0800

    exercise 2

---
+++ ex2/test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def test():
+    #l = range(20)
+    l = [[1, 3], [4, 5], [5, 7]]
+    m = len(l)
+    lis =  np.ones((m,2))
+    print(lis)
+
+def main():
+    test()
+
+if __name__ == "__main__":
+    main()
